cc9427d776be7f655b14043322156669fc326214
AlongwithKiman <expfunsce@snu.ac.kr>
2019-11-26 23:40:45 +0900

    init

---
+++ front/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice',major:'a' },
+  { id: 12, name: 'Narco',major:'a' },
+  { id: 13, name: 'Bombasto',major:'a' },
+  { id: 14, name: 'Celeritas' ,major:'a'},
+  { id: 15, name: 'Magneta',major:'a' },
+  { id: 16, name: 'RubberMan',major:'a' },
+  { id: 17, name: 'Dynama',major:'a' },
+  { id: 18, name: 'Dr IQ',major:'a' },
+  { id: 19, name: 'Magma',major:'a' },
+  { id: 20, name: 'Tornado',major:'a' }
+];
